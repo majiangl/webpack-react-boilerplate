33b8da9b5a1adad3402355279987b73b271e413b
马江龙 <06rjgcmjl@gmail.com>
2018-04-15 20:43:40 +0800

    add jsdoc

--- src/component/DemoComponent/DemoComponent.js
+++ src/component/DemoComponent/DemoComponent.js
@@ -1,19 +1,38 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 /**
- * Use jsdoc syntax to describe Class and API. And use docco syntax
- * to explain code logic.
+ * React Component
+ *
+ * @author Ma Jianglong <rjgcmjl@163.com>
  */
-class Demo extends Component {
-  // This is [docco](http://ashkenas.com/docco/) style comment which is parsed as **markdown**.
-  // Docco is used to explain code logic. And we can use docco to generate more readable docs.
+class DemoComponent extends Component {
+  /**
+   * Properties passed to react component.
+   *
+   * @alias DemoComponent#props
+   * @property text {string} - Required. Text to display
+   */
+  static propTypes = {
+    text: PropTypes.string.isRequired
+  }
+
+  /**
+   * Create a react component
+   * @param {object} props - properties passed to react component
+   */
+  constructor(props) {
+    super(props);
+  }
+
   render() {
+    const {text} = this.props;
     return (
       <div>
-        This is a demo component.
+        {text}
       </div>
     );
   }
 }
 
-export default Demo;
+export default DemoComponent;
